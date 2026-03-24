d3a8d229516bcbb8db12bf5a3e06634565e182e2
Dylan Palmboom <nofxboy1234@gmail.com>
2026-03-24 13:40:43 +0200

    Add stale time to query

--- apps/web/src/queries/notes.ts
+++ apps/web/src/queries/notes.ts
@@ -4,4 +4,5 @@ import { getNotes } from "@node-map/api-client";
 export const notesQuery = queryOptions({
   queryKey: ["notes"],
   queryFn: () => getNotes(import.meta.env.VITE_API_URL),
+  staleTime: 30_000,
 });
