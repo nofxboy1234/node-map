82c91e7c8aab65842000bdd82208a21aec4500f3
Dylan Palmboom <nofxboy1234@gmail.com>
2026-04-07 08:30:32 +0200

    Move off vite-plus to its separate deps
    Vite-plus is still in alpha, so there are a few issues. Can have a look at it later.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite-plus";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tanstackRouter(), react(), cloudflare()],
-  staged: {
-    "*": "vp check --fix",
-  },
-  lint: {
-    ignorePatterns: ["src/routeTree.gen.ts", "migrations/**"],
-    options: { typeAware: true, typeCheck: true },
-  },
-  fmt: {
-    ignorePatterns: ["src/routeTree.gen.ts", "migrations/**"],
-  },
 });
